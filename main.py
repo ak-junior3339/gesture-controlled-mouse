d2f013eae67ed617f03381f662e40a96992cb4e8
AK-junior <codeninjak27@gmail.com>
2026-08-05 12:48:52 +0530

    Implementing Mediapipe and drawing landmarks

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import cv2
 import mediapipe as mp
 
 mpHands = mp.solutions.hands
-hands = mp.mpHands.Hands(
+hands = mpHands.Hands(
     # we are capturing video so no static 
     static_image_mode = False,
     model_complexity = 1,
@@ -16,6 +16,7 @@ hands = mp.mpHands.Hands(
 def main():
     # Setting up the camera to capture video
     cap = cv2.VideoCapture(0)
+    draw = mp.solutions.drawing_utils
     try:
         while cap.isOpened():
             # 'ret' will contain true or false depending upon the cap.read()
@@ -25,6 +26,17 @@ def main():
                 break
             # Flipping the image for mirror image so that right <-> left dosent get interchanged
             frame = cv2.flip(frame,1)
+            # Open cv captures the frame by default in BGR format but to process 
+            #it by mrdiapipr we need to convert it to RGB format 
+            frameRGB = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+            # Now processing the RGB frame to detect all the 21 landmarks from frame
+            processed = hands.process(frameRGB)
+            landmarks_list = []
+            if processed.multi_hand_landmarks:
+                # If the processed frame captures 2 hands make it 1 and the respective landmarks
+                hand_landmarks = processed.multi_hand_landmarks[0]
+                # Drawing the Hand Landmarks with connections in thr open CV's BGR frame
+                draw.draw_landmarks(frame,hand_landmarks,mpHands.HAND_CONNECTIONS)
             # Showing the frame on screen 
             cv2.imshow('Frame',frame)
             # Code for waiting 1 ms and if q is pressed then quit
